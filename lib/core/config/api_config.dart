a61e097821aa63b44ee305943f75f22b20f277e6
Rushi-Parekar11 <rushikeshparekar11@gmail.com>
2026-08-03 16:06:32 +0530

    Empty main branch code

--- lib/core/config/api_config.dart
+++
@@ -1,5 +0,0 @@
-class ApiConfig {
-  // Use localhost since we are testing on Chrome.
-  // For Android emulator, this should be 'http://10.0.2.2:3109/api/v1/sachi'
-  static const String baseUrl = 'http://localhost:3109/api/v1/sachi';
-}
